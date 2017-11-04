44fc6035a028ca2359cdca17639692f9696ef6f1
Dmitry Hontar <dmitryhontar@gmail.com>
2017-11-04 13:01:01 +0200

    fixes task3

--- task3/js/scripts.js
+++ task3/js/scripts.js
@@ -1,18 +1,14 @@
 var arr = [1, 2, 3, 4 ,5 , 6, 7];
-function forEach(arr, element){
+function forEach(arrey, callback){
 	var i = 0;
-	var value;
-	while(i < arr.length){
-		if(arr[i] === element) { 
-			value = element;
-			break;
-		}
+	while(i < arrey.length){
+		callback(array[i], i , array)
 		i++;
+		}
 	}
-	console.log(value);
-	return value;
 }
 
+forEach(arr);
 forEach(arr, 2)
 	
 		
